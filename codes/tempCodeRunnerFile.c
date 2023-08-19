4846730de6459b1d01c1987e5a837cddffdc5b33
whoshyam <shyamk2712@gmail.com>
2023-08-19 15:55:28 +0530

    yo

---
+++ codes/tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+   // int age;
+    // printf("\nenter your age:");
+    // scanf("%d, &age");
+
+    // if (age>=18){
+    //     printf("you are signed up!");
+    // }
+    // else if(age==0){
+    //     printf("no sign up");
+    // }
+    // else if(age<0){
+    //     printf("yet to born");
+    // }
+    // // sequencce wise if ya else if ya dusra else if ka contion meet hogya th uske baad ke condition ko check nhi hoga
+    // else{
+    //     printf("you are too young");
+    // }
