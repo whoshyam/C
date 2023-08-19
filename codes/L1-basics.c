ef89dd0dff5856fcff508acbfa5d2e152cd8dd98
whoshyam <shyamk2712@gmail.com>
2023-08-19 14:14:28 +0530

    hello

--- codes/L1-basics.c
+++ codes/L1-basics.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
     // print statement
@@ -34,10 +36,9 @@ int main()
 
     // bool e = true; //%d
 
-    char a = 'C';         // %c
-    char f = 120;         // 1 byte (-128 to +127)
+    char a = 'C';          // %c
+    char f = 120;          // 1 byte (-128 to +127)
     unsigned char g = 225; // 1 byte ( 0 tp +255) %d or %c
-
 
     // format specifer
     //%.1 = decimal precision
@@ -60,36 +61,56 @@ int main()
     printf("item 1: $%8.2f\n", item3);
     // item 1: $    5.75
     // item 1: $   10.00
-    // item 1: $  100.99 
+    // item 1: $  100.99
 
-
-    //constants: fixed value that cannot be altered  
-    const float pi =3.14159;
-    //pi = 233 // this cannot be done now 
+    // constants: fixed value that cannot be altered
+    const float pi = 3.14159;
+    // pi = 233 // this cannot be done now
     printf("%f\n", pi);
 
-
-    //operators
+    // operators
 
     int x = 5;
     int y = 2;
-    float z = x/y; // z=2
-    float z1 = x / (float) y; //z=2.500000
+    float z = x / y;         // z=2
+    float z1 = x / (float)y; // z=2.500000
     printf("%f", z);
 
-    int v = x % y; //remainder 
+    int v = x % y; // remainder
     printf("%d", z);
 
-    int value =10;
-    //value = value + 2;
-    value+=2;
+    int value = 10;
+    // value = value + 2;
+    value += 2;
 
-    //user input 
+    // user input
 
+    char name1[25];
+    int age1;
 
+    printf("\nwhat's your name1?\n");
+    scanf("%s", &name1); // use input for single world ex shyam or ram but not shyam kumar
+    // so to get multiple word user input we use ..
+    fgets(name1, 25, stdin); // fgets(variableName, limit, stdin);
 
+    printf("\nHow old are you?");
+    scanf("%d", &age1);
 
+    printf("Hello %s,how are you\n", name1);
+    printf("you are %d years old\n", age1);
 
+    // math functions
+
+    double A = sqrt(9);
+    double B = pow(2, 4);
+    int C = round(3.14);   // round to 3
+    int D = ceil(3.14);    // round to 4
+    int E = floor(3.99);   // round to3
+    double F = fabs(-100); // absolute value
+    double G = log(3);
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(45);
 
     return 0;
 }
