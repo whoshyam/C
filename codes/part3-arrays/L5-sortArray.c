2e0a0733cc7b6e5df5cb53e625880eff1d6247ae
whoshyam <shyamk2712@gmail.com>
2023-08-22 16:47:57 +0530

    yooo

--- codes/part3-arrays/L5-sortArray.c
+++ codes/part3-arrays/L5-sortArray.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
+void sort(int array[], int size)
+{
+    for (int i = 0; i < size - i - 1; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printarray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+}
+
 int main()
 {
-    
+    int array[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};
+    int size = sizeof(array) / sizeof(array[0]);
+
+    sort(array, size);
+    printarray(array, size);
 }
