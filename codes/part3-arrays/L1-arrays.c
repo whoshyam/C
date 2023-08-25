db996e3504d799ec98eb442e44bf094eb3b2ad84
whoshyam <shyamk2712@gmail.com>
2023-08-25 19:34:23 +0530

    j

--- codes/part3-arrays/L1-arrays.c
+++ codes/part3-arrays/L1-arrays.c
@@ -33,3 +33,20 @@ int main()
 
     return 0; 
 }
+
+//There is no array.length for C arrays.
+
+//sizeof(arr) / sizeof(arr[0]) mean
+
+// sizeof(arr)
+// yields the number of bytes occupied by array arr (I suppose that arr is an array).
+// For example if you declared an array like
+// int arr[10];
+// then the compiler has to reserve memory that to hold 10 elements of type int. If for example sizeof( int ) is equal to 4 then the compiler will reserve 10 * 4 = 40 bytes of memory.
+
+// sizeof(arr[0])
+// gives the number of bytes occupied by one element in the array. You could use any index as for example
+//sizeof(arr[0]) it means: 1 * 4 = 4
+
+//sizeof(arr) / sizeof(arr[0]) = 10*4 / 1*4 = 10, and it is the length of the array.
+
